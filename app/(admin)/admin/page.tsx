e6b42b2fa1aec4f898d739c8a80a377f3ee59f7c
Jahid0178 <zahidul.haque767@gmail.com>
2023-07-10 21:32:36 +0600

    mod - modify admin and client layouts

---
+++ app/(admin)/admin/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AdminDashboard from "./dashboard/page";
+
+const AdminPanel = () => {
+  return (
+    <>
+      <AdminDashboard />
+    </>
+  );
+};
+
+export default AdminPanel;
