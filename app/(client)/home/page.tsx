76ab934b93e394cd5421156f959f9989e7180a7b
Jahid0178 <zahidul.haque767@gmail.com>
2023-07-13 07:05:58 +0600

    mod - added home page promotion banner section

--- app/(client)/home/page.tsx
+++ app/(client)/home/page.tsx
@@ -1,12 +1,39 @@
 "use client";
 
+import Button from "@/components/common/Button/Button";
+import Image from "next/image";
+
 const HomePage = () => {
   return (
-    <section>
+    <section className="section-bg">
       <div className="container">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div>1</div>
-          <div>2</div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
+          <div>
+            <h3 className="text-secondary mb-4">
+              Best Furniture For Your Castle....
+            </h3>
+            <h1 className="mb-4 text-4xl md:text-6xl font-bold">
+              New Furniture Collection Trends in 2023
+            </h1>
+            <p className="mb-4 text-dimm">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Accusamus, totam veniam! Ad animi harum suscipit eveniet magni
+              expedita modi adipisci?
+            </p>
+            <Button type="button" className="btn btn-primary">
+              Shop Now
+            </Button>
+          </div>
+          <div>
+            <Image
+              src="/images/sofa-promotional-header.png"
+              alt="hero banner image"
+              width={800}
+              height={800}
+              className="mx-auto"
+              draggable={false}
+            />
+          </div>
         </div>
       </div>
     </section>
