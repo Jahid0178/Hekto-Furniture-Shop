5b88e14fc02fab3482f03f24a9c31d0d866730ba
Jahid0178 <zahidul.haque767@gmail.com>
2023-07-16 03:51:21 +0600

    add - added offer data in home page and create offer card component

---
+++ components/client/Cards/OfferCard/OfferCard.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+type OfferCardProps = {
+  data: {
+    id: number;
+    title: string;
+    desc: string;
+    image: string;
+  };
+};
+
+const OfferCard = ({ data }: OfferCardProps) => {
+  const { title, desc, image } = data;
+  return (
+    <div className="text-center p-4 shadow">
+      <Image
+        src={image}
+        alt={title}
+        width={60}
+        height={60}
+        className="mb-4 mx-auto"
+      />
+      <h2 className="mb-2 text-2xl text-primary">{title}</h2>
+      <p>{desc}</p>
+    </div>
+  );
+};
+
+export default OfferCard;
