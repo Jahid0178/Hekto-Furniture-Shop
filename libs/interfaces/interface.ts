8a24d4e360f1473450653011b2f6f64a24c85d23
Jahid0178 <zahidul.haque767@gmail.com>
2023-07-22 02:14:42 +0600

    mod - added contact form in contact us page

--- libs/interfaces/interface.ts
+++ libs/interfaces/interface.ts
@@ -2,3 +2,10 @@ export interface LogInFormInput {
   email: string;
   password: string;
 }
+
+export interface ContactFormProps {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
