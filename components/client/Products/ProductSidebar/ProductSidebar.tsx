0aedcfa0b39904f9d852c382425ed7ce816e09e4
Jahid0178 <zahidul.haque767@gmail.com>
2023-07-22 00:08:46 +0600

    mod - modify products sidebar title and items

--- components/client/Products/ProductSidebar/ProductSidebar.tsx
+++ components/client/Products/ProductSidebar/ProductSidebar.tsx
@@ -9,12 +9,16 @@ const ProductSidebar = () => {
       {sidebarContents.map((content, ind) => {
         return (
           <div key={ind} className="mb-4">
-            <h2 className="text-lg text-primary mb-2">{content.title}</h2>
+            <h2 className="text-lg text-primary mb-2 font-bold underline underline-offset-4">
+              {content.title}
+            </h2>
             <ul>
               {content.items.map((item) => (
                 <li key={item.id} className="flex items-center gap-2">
                   <input type="checkbox" id={item.label} />
-                  <label htmlFor={item.label}>{item.label}</label>
+                  <label htmlFor={item.label} className="text-[#7E81A2]">
+                    {item.label}
+                  </label>
                 </li>
               ))}
             </ul>
