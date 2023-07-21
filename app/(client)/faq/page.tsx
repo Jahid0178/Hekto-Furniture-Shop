9e46d765e17f255bab062e5b97054ae6bf66924e
Jahid0178 <zahidul.haque767@gmail.com>
2023-07-22 02:49:17 +0600

    add - ask question form in faq page

--- app/(client)/faq/page.tsx
+++ app/(client)/faq/page.tsx
@@ -1,14 +1,47 @@
+"use client";
+
+import React from "react";
+import Heading from "@/components/client/Heading/Heading";
 import Container from "@/components/common/Container";
 import PageHeading from "@/components/common/PageHeading/PageHeading";
-import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { FAQFormProps } from "@/libs/interfaces/interface";
 
 const FAQPage = () => {
+  const { register, handleSubmit } = useForm<FAQFormProps>();
+  const onSubmit: SubmitHandler<FAQFormProps> = (data) => console.log(data);
   return (
     <>
       <PageHeading title="FAQ" />
       <section>
         <Container>
-          <h1>FAQ Page</h1>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div className="p-2">
+              <Heading title="General Information" />
+            </div>
+            <div className="p-2">
+              <Heading title="Ask a Question" />
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                  {...register("name", { required: true })}
+                  placeholder="Your Name*"
+                  className="w-full border mb-4 p-2 outline-none"
+                />
+                <input
+                  {...register("subject", { required: true })}
+                  placeholder="Subject*"
+                  className="w-full border mb-4 p-2 outline-none"
+                />
+                <textarea
+                  {...register("message", { required: true })}
+                  placeholder="Type Your Message*"
+                  rows={5}
+                  className="w-full border mb-4 p-2 outline-none"
+                />
+                <input type="submit" className="btn btn-primary" />
+              </form>
+            </div>
+          </div>
         </Container>
       </section>
     </>
