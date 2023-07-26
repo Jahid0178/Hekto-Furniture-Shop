09f68d04e757b9efb4a95f57cff3a017fb4e1da2
Jahid0178 <zahidul.haque767@gmail.com>
2023-07-27 02:04:06 +0600

    Modify Add Product Page
    - Added Few Product Inputs To Add Product

--- app/(admin)/admin/add-product/page.tsx
+++ app/(admin)/admin/add-product/page.tsx
@@ -1,13 +1,167 @@
 "use client";
 
+import React, { useState } from "react";
 import Container from "@/components/common/Container";
-import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { AddProductInputProps } from "@/libs/types/types";
+
+type TagsProps = string[];
 
 const AddProductPage = () => {
+  const [tags, setTags] = useState<TagsProps>([]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<AddProductInputProps>();
+  const onSubmit: SubmitHandler<AddProductInputProps> = (data) => {
+    const modifyData = { ...data, tags };
+    console.log(modifyData);
+  };
+
+  const handleTags = (e: any) => {
+    const tagsValue = e.target.value;
+    setTags((prev) => [...prev, tagsValue]);
+  };
+
   return (
     <section>
       <Container>
-        <h2>Add Product</h2>
+        <div>
+          <h1 className="text-2xl">Add Product</h1>
+          <form onSubmit={handleSubmit(onSubmit)} className="mt-4">
+            <div className="mb-2">
+              <label className="mb-1 inline-block" htmlFor="images">
+                Images
+              </label>
+              <br />
+              <input
+                id="images"
+                type="file"
+                placeholder="Images"
+                multiple={true}
+                accept="image/*"
+                {...register("images", { required: true })}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="mb-1 inline-block" htmlFor="name">
+                Product Name
+              </label>
+              <br />
+              <input
+                id="name"
+                type="text"
+                placeholder="Product Name"
+                {...register("productName", { required: true })}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="mb-1 inline-block" htmlFor="price">
+                Price
+              </label>
+              <br />
+              <input
+                id="price"
+                type="number"
+                placeholder="Price"
+                {...register("price", { required: true })}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="mb-1 inline-block" htmlFor="category">
+                Category
+              </label>
+              <br />
+              <input
+                id="category"
+                type="text"
+                placeholder="Category"
+                {...register("category", { required: true })}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="mb-1 inline-block" htmlFor="quantity">
+                Quantity
+              </label>
+              <br />
+              <input
+                id="quantity"
+                type="text"
+                placeholder="Quantity"
+                {...register("quantity", { required: true })}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="mb-1 inline-block" htmlFor="brand">
+                Brand
+              </label>
+              <br />
+              <input
+                id="brand"
+                type="text"
+                placeholder="Brand"
+                {...register("brand", { required: true })}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="mb-1 inline-block" htmlFor="desc">
+                Description
+              </label>
+              <br />
+              <textarea
+                id="desc"
+                cols={30}
+                rows={10}
+                placeholder="Product Description"
+                {...register("description", { required: true })}
+              />
+            </div>
+
+            <div className="mb-2">
+              <label className="mb-1 inline-block" htmlFor="tags">
+                Tags
+              </label>
+              <br />
+              <select id="tags" onChange={handleTags}>
+                <option value="new">New</option>
+                <option value="features">Features</option>
+                <option value="new arrival">New Arrival</option>
+                <option value="discounted">Discounted</option>
+                <option value="flash sale">Flash Sale</option>
+                <option value="exclusive">Exclusive</option>
+                <option value="best sell">Best Sell</option>
+                <option value="trending">Trending</option>
+                <option value="hot deal">Hot Deal</option>
+              </select>
+            </div>
+
+            <div className="my-4">
+              <ul className="flex gap-4">
+                {tags.map((tg, ind) => (
+                  <li
+                    key={ind}
+                    className="border border-gray-500 py-1 px-2 rounded cursor-pointer"
+                  >
+                    {tg}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <input
+              type="submit"
+              value="Add Product"
+              className="btn-secondary"
+            />
+          </form>
+        </div>
       </Container>
     </section>
   );
