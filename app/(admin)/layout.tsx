2e08e77924129d700764936dc203dfbe3a0f3743
Jahid0178 <zahidul.haque767@gmail.com>
2023-07-29 20:33:11 +0600

    Added hot toast in admin layout
    - Added react hot toast showing after product created

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -2,6 +2,7 @@ import AdminSidebar from "@/components/admin/AdminSidebar/AdminSidebar";
 import "../globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
+        <Toaster />
         <main className="grid  grid-cols-[300px_minmax(900px,_1fr)]">
           <AdminSidebar />
           {children}
